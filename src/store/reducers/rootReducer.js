402b8154900e9dd529cf085616d80f14d93013f4
rathtana <drathtana@gmail.com>
2020-10-28 23:31:15 -0700

    delete key from chil projectList, attached firebase to rootreducer and navbar

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,12 +3,16 @@ import projectReducer from './projectReducer';
 import { combineReducers } from 'redux';
 //syncing Data with Firestore 
 import { firestoreReducer } from 'redux-firestore';
+//syncing Data with Firebase for auth 
+import { firebaseReducer } from 'react-redux-firebase'
 
 const rootReducer = combineReducers({
     auth: authReducer,
     project: projectReducer,
     //syncing data with firestore 
-    firestore: firestoreReducer
+    firestore: firestoreReducer,
+    //syncing data with firebase for auth 
+    firebase: firebaseReducer
 })
 
 export default rootReducer;
